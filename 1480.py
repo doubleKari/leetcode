3b4d7918d2536837b3b095eb9ef67e4e769c1fb6
doublekari <kaykayyaw@gmail.com>
2023-07-15 16:06:57 +0000

    initial commit

---
+++ 1480.py
@@ -0,0 +1,17 @@
+# running_sums = []
+# nums = [2,7,9,8]
+# prev_sum = 0
+# for i in range(len(nums)):
+#     prev_sum += nums[i]
+#     running_sums.append(prev_sum)
+
+# print(running_sums)
+
+#################################
+
+nums = [3,1,2,10,1]
+#Alternative solution
+for i in range(1,len(nums)):
+    nums[i]+=nums[i-1]
+
+print(nums)
---
+++ README.md
@@ -0,0 +1,4 @@
+#LEETCODE CHALLENGES
+
+Here are solutions to problems from leetcode that I've tried.
+I'm no true programmer yet and that is what I strive to be. 
